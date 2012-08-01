cefc427c9edb9d7e47cbbed602294aa5871b87a0
Anthony Accomazzo <accomazz@gmail.com>
2012-08-01 13:17:18 -0700

    throw a 400 for bad requests

--- lib/erroneous-api/server.rb
+++ lib/erroneous-api/server.rb
@@ -18,14 +18,18 @@ module ErroneousAPI
     end
 
     post '/parse_deploy_fail' do
-      deploy_log = JSON.parse(request.body.read)["deploy_log"]
-      content_type :json
-      result = ErroneousAPI::Server.mapper.parse_deploy_fail(deploy_log)
-      {
-        :lines => (result[:lines] or raise "Expected parse_deploy_fail to return which :lines (empty array allowed)").to_a,
-        :details => (result[:details] or raise "Expected parse_deploy_fail to return the :details (empty string allowed)"),
-        :summary => (result[:summary] or raise "Expected parse_deploy_fail to return the :summary (empty string allowed)")
-      }.to_json
+      begin
+        deploy_log = JSON.parse(request.body.read)["deploy_log"]
+        content_type :json
+        result = ErroneousAPI::Server.mapper.parse_deploy_fail(deploy_log)
+        {
+          :lines => (result[:lines] or raise "Expected parse_deploy_fail to return which :lines (empty array allowed)").to_a,
+          :details => (result[:details] or raise "Expected parse_deploy_fail to return the :details (empty string allowed)"),
+          :summary => (result[:summary] or raise "Expected parse_deploy_fail to return the :summary (empty string allowed)")
+        }.to_json
+      rescue JSON::ParserError
+        status 400
+      end
     end
   end
 end
